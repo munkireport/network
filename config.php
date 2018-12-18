c7fa9987f29d090b567e33ad2142959cad1a1a57
Arjen van Bochoven <nejra@me.com>
2018-12-18 23:21:23 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-  'router_config_path' => env('NETWORK_ROUTER_CONFIG_PATH', APP_ROOT . 'local/module_configs/ipv4routers.yml'),
+  'router_config_path' => env('NETWORK_ROUTER_CONFIG_PATH', module_conf('ipv4routers.yml')),
 ];
